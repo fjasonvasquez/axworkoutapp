70bbe6cd9173cd5482bc2beec9eb450daf091fbf
fjasonvasquez <fjvasquez@aol.com>
2017-11-06 20:28:50 -1000

    layouts, and signing in is updated for easier user experience

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 RSPec.feature "Users signup" do
+	before do
+		@john = User.create!(email: "john@example.com", 
+												 password; "password")
+
+	end
+
 	scenario "with valid credentials" do 
 		visit "/"
-		
+
 		click_link "Sign up"
 		fill_in "Email", with: "john@example.com"
 		fill_in "Password", with: "password"
@@ -11,5 +17,6 @@ RSPec.feature "Users signup" do
 		click_button "Sign up"
 
 		expect(page).to have_content("You have signed up successfully.")
+		expect(page).to have_content("Signed in as #{(@john.email)}")
 	end
 end
