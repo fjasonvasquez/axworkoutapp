cb161b51b8440a2df99090215c406ef844998500
fjasonvasquez <fjvasquez@aol.com>
2017-12-07 23:40:06 -1100

    Add pagination to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   has_many :exercises
 
   validates :first_name, presence: true 
-  validates :last_name, presence: true 
+  validates :last_name, presence: true
+
+  self.per_page = 10 
 
   def full_name
   	"#{first_name} #{last_name}"
