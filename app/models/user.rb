523799d972c7ad95dbe93e392c11c5818d060464
fjasonvasquez <fjvasquez@aol.com>
2017-12-09 20:36:43 -1100

    Write following friends feature test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,35 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         
   has_many :exercises
-
-  validates :first_name, presence: true 
+  has_many :friendships
+  has_many :friends, through: :friendships, class_name: "User"
+  has_one :room
+  has_many :messages
+  
+  validates :first_name, presence: true
   validates :last_name, presence: true
-
-  self.per_page = 10 
-
+  
+  after_create :create_chatroom
+  
+  self.per_page = 10
+  
   def full_name
-  	"#{first_name} #{last_name}"
+    [first_name, last_name].join(" ")
+  end
+  
+  def self.search_by_name(name)
+    names_array = name.split(' ')
+
+    if names_array.size == 1
+      where('first_name LIKE ? or last_name LIKE ?',
+        "%#{names_array[0]}%", "%#{names_array[0]}%").order(:first_name)
+    else
+      where('first_name LIKE ? or first_name LIKE ? or last_name LIKE ?
+        or last_name LIKE ?', "%#{names_array[0]}%",
+        "%#{names_array[1]}%", "%#{names_array[0]}%",
+        "%#{names_array[1]}%").order(:first_name)
+    end
   end
 end
