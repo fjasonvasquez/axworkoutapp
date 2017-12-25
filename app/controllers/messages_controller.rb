d69b8ecbb52ef97bb3853992b97fcad08faa3c32
fjasonvasquez <fjvasquez@aol.com>
2017-12-24 20:04:23 -1100

    fixed app controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,9 @@
-class MessagesController < ApplicationController
-	before_action :authenticate_user!
-
-	def create
-		@message = current_user.messages.build(message_params)
-		@nessage.room = current_room
-
-		@message.save
-		redirect_to user_exercises_path(current_user)
-	end
-
-	private
-
-	def message_params
-		params.require(:message).permit(:body)
-	end
-end
+ if @message.save
+        respond_to do |format|
+          format.html { redirect_to user_exercises_path(current_user) }
+ -        format.js { ActionCable.server.broadcast "messages_room_#{current_room.id}",
+ -        render(partial: 'shared/message', object: @message) }
+ +        format.js { ActionCable.server.broadcast "messages_room_#{current_room.id}", render(partial: 'shared/message', object: @message) }
+        end
+      end
+    end
